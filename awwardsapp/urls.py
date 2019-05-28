f499fcbcb7c668bdff821c65f1feef75ee385a30
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-28 15:29:43 +0300

    Add changes made in the views.py & urls.py

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url
 from . import views
 
-urlpattern=[
+urlpatterns = [
     url('^$',views.index,name = 'index'),
 ]
--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
+import datetime as dt
+
 
 # Create your views here.
 def index(request):
