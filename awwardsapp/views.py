0c9736a67abb71f6613564aeff3e13812a05e289
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-29 09:08:14 +0300

    Create a view function for the search results

--- awwardsapp/views.py
+++ awwardsapp/views.py
@@ -6,3 +6,6 @@ import datetime as dt
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def search_results(request):
+    return render(request, 'search.html')    
