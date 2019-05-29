a241b23de1fe0fd64fc41d1ca953d3d6fe43e927
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-29 17:07:06 +0300

    Update the settings and urls to enable us upload images

--- awwardsapp/urls.py
+++ awwardsapp/urls.py
@@ -13,3 +13,5 @@ urlpatterns=[
     url(r'^api/post/$', views.PostList.as_view()),
     url(r'^api/profile/$', views.ProfileList.as_view())
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
