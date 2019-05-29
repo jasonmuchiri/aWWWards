2718aa7429b830f6e01d4ae2b6f430abeefd46c7
jasonmuchiri <jasonmkinyua@gmail.com>
2019-05-29 11:35:50 +0300

    Create an admin page and add the models to the admin page , Update the models.py file

--- awwardsapp/models.py
+++ awwardsapp/models.py
@@ -14,7 +14,7 @@ class Profile(models.Model):
 
 
     @receiver(post_save, sender=User)
-    def create_user_profile(self,sender, instance, created, **kwargs):
+    def create_user_profile(sender, instance, created, **kwargs):
         if created:
             Profile.objects.create(user=instance)
 
